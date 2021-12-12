40f92967050aedf9b0ea31bb6b34995fd3e0f35e
simana9 <94266502+simana9@users.noreply.github.com>
2021-12-11 20:41:11 -0500

    Update netsize.sh
    Modifications the words

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -1,11 +1,11 @@
 ## Ejercicio Parte 1
 1.Ubicarse en la ruta cd Documents/GBI/CSB/unix/data/Saavedra2013 
-2.Escribimos en el terminal
+2.Escribir en el terminal
 cat n1.txt
-#Entonces se imprimió todo lo que contenia Saavedra2013
-echo "El ńumero de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
+#Se imprimió todo lo que contenia Saavedra2013
+echo "El número de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
 #Como resultado final
-#El ńumero de columnas:
+#El número de columnas:
 80
 #El numero de filas:
 97 n1.txt
