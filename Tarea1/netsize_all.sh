f21e2768289447c6df10d853ededeadb2deb7c17
simana9 <94266502+simana9@users.noreply.github.com>
2021-12-11 20:45:35 -0500

    Update netsize_all.sh
    Modify the text

--- Tarea1/netsize_all.sh
+++ Tarea1/netsize_all.sh
@@ -1,9 +1,9 @@
 #2.Ejercicio 1.10.3 Part 2
- Especificado en el README.md
- -Elegimos las misma ruta del repositorio anterior
+ Especificado en README.md
+ -Elegir la misma ruta del repositorio anterior
  Corremos la información 
  cd Saavedra2013
- #Ejecutaremos el codigo en git bush
- Es esta parte utilizaremos el for , do , done
+ #Ejecución el codigo en git bush
+ Utilizaremos el for , do , done
  for file in $(ls *.txt); do wc -l $file; head -n1 $file | grep -o " " | wc -l; done
 # Finalizar obtenemos los resultados de netsize_all.txt con archivos de n59.txt
