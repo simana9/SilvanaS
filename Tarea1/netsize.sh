96a29ba8dc0698fe46aa4efdd15a1ea38d0da56a
simana9 <94266502+simana9@users.noreply.github.com>
2021-12-11 20:44:01 -0500

    Update netsize.sh
    Modify the text

--- Tarea1/netsize.sh
+++ Tarea1/netsize.sh
@@ -2,7 +2,7 @@
 1.Ubicarse en la ruta cd Documents/GBI/CSB/unix/data/Saavedra2013 
 2.Escribir en el terminal
 cat n1.txt
-#Se imprimió todo lo que contenia Saavedra2013
+#Se imprime todo el contenido de Saavedra2013
 echo "El número de columnas es:" ; head -n1 n1.txt | grep -o " " | wc -l ; echo "El numero de filas es:" ; wc -l n1.txt; echo "Fin de la ejecucion"
 #Como resultado final
 #El número de columnas:
